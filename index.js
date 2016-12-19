70b732e53d6badbcef1f633688e15654c16ee39a
subo <susann.bombor@zebware.com>
2016-12-19 13:56:47 +0100

    Add clock center

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ function setDate(secondHand, minuteHand, hourHand) {
     let secondsDegrees = ((seconds / 60) * 360) + 90;
     let minutesDegrees = ((minutes / 60) * 360) + 90;
     let hoursDegrees = ((hours / 12) * 360) + 90;
-    // console.log(seconds + " " + secondsDegrees);
+    console.log(seconds + " " + secondsDegrees);
     //rotation every second
     secondHand.style.transform = (`rotate(${secondsDegrees}deg)`);
     minuteHand.style.transform = (`rotate(${minutesDegrees}deg)`);
     hourHand.style.transform = (`rotate(${hoursDegrees}deg)`);
+    // 444 auf 90 360 + 90 = 450
 }
 
 window.onload = function () {
